e079ac83dc61fdb27f0a761cf51fc6cc3431b797
nishan <nishanpradhan500@gmail.com>
2025-08-23 16:05:13 +0545

    learnt to blink led

--- blink_onboard_led/blink_onboard_led.ino
+++ blink_onboard_led/blink_onboard_led.ino
@@ -1,19 +1,6 @@
 
 
-int ledPin = 2; // onboarad LED on GPIO23
-
-void setup() {
-  pinMode(ledPin, OUTPUT);
-}
-
-void loop() {
-  digitalWrite(ledPin, HIGH); // LED ON
-  delay(1000);                 // wait half second
-  digitalWrite(ledPin, LOW);  // LED OFF
-  delay(1000);                 // wait half second
-}
-
-// int ledPin = 23; // external LED on GPIO23
+// int ledPin = 2; // onboarad LED on GPIO23
 
 // void setup() {
 //   pinMode(ledPin, OUTPUT);
@@ -25,3 +12,16 @@ void loop() {
 //   digitalWrite(ledPin, LOW);  // LED OFF
 //   delay(1000);                 // wait half second
 // }
+
+int ledPin = 23; // external LED on GPIO23
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // LED ON
+  delay(1000);                 // wait half second
+  digitalWrite(ledPin, LOW);  // LED OFF
+  delay(1000);                 // wait half second
+}
